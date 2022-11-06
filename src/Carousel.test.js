1884e083c02e8da57d201125edac9dd4dff650ba
Patrick Sontag <pmsontag84@gmail.com>
2022-11-06 14:12:37 -0600

    remove unnecessay arrows

--- src/Carousel.test.js
+++ src/Carousel.test.js
@@ -1,4 +1,4 @@
-import { fireEvent, getByTestId, render, screen } from '@testing-library/react';
+import { fireEvent, getByTestId, queryByTestId, render, screen } from '@testing-library/react';
 import Carousel from './Carousel';
 
 it ("should render", () => {
@@ -12,15 +12,21 @@ it("should match snapshot", () => {
 
 it("should show next/previous image when right/left arrow clicked", () => {
     const { getByText } = render(<Carousel />);
-    const textCard1 = getByText("Photo by Richard Pasquarella on Unsplash");
+    const cardText = getByText("Photo by Richard Pasquarella on Unsplash");
     const arrowRight = screen.getByTestId("right-arrow");
-    expect(textCard1).toHaveTextContent("Photo by Richard Pasquarella on Unsplash");
+    expect(cardText).toHaveTextContent("Photo by Richard Pasquarella on Unsplash");
     fireEvent.click(arrowRight);
     const arrowLeft = screen.getByTestId("left-arrow");
-    const textCard2 = getByText("Photo by Pratik Patel on Unsplash");
-    expect(textCard1).not.toHaveTextContent("Photo by Richard Pasquarella on Unsplash");
-    expect(textCard1).toHaveTextContent("Photo by Pratik Patel on Unsplash");
+    expect(cardText).not.toHaveTextContent("Photo by Richard Pasquarella on Unsplash");
+    expect(cardText).toHaveTextContent("Photo by Pratik Patel on Unsplash");
     fireEvent.click(arrowLeft);
-    expect(textCard1).toHaveTextContent("Photo by Richard Pasquarella on Unsplash");
-    expect(textCard1).not.toHaveTextContent("Photo by Pratik Patel on Unsplash");
+    expect(cardText).toHaveTextContent("Photo by Richard Pasquarella on Unsplash");
+    expect(cardText).not.toHaveTextContent("Photo by Pratik Patel on Unsplash");
+})
+
+
+it("should hide left arrow when on first image", () => {
+    const { queryByTestId } = render(<Carousel />);
+    const arrowLeft = queryByTestId("left-arrow");
+    expect(arrowLeft).toBeNull();
 })
